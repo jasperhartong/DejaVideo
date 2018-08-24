30fad5ad0973b17b0a3afa60a6c8003074b8d646
Jasper Hartong <Jasper@calendar42.com>
2018-08-24 17:54:04 +0200

    Initial working menubar app

--- ContinuousRecorder/AppDelegate.swift
+++ ContinuousRecorder/AppDelegate.swift
@@ -10,16 +10,36 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var item : NSStatusItem? = nil
 
     @IBOutlet weak var window: NSWindow!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        //Displays text on menu bar
+        item?.image = NSImage(named: NSImage.Name(rawValue: "MenuRec"))
+        
+        // Defines function to run when the user clicks on the text on menu bar
+        //item?.action = #selector(AppDelegate.testMe)
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Option 1", action: #selector(AppDelegate.testMe), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quitMe), keyEquivalent: ""))
+        item?.menu = menu
+
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    @objc func testMe() {
+        print ("It works!")
+    }
+    
+    @objc func quitMe() {
+        NSApplication.shared.terminate(self)
     }
 
 
